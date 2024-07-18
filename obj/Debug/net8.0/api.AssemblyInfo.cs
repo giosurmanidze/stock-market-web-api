a197f04f13a286de6ca05c3cc9c8a404482fbb30
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-07-18 18:41:17 +0400

    feat/connect to sql server database  install ef

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48eb3d6b5c2cb9121351315079a6d5e2889a7283")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8613846bdef53f81f5d5af9c7d06941717959da6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
